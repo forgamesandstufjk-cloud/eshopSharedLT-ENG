e07ccdb8f2cdd118ab32cba14a00a3b0283a7805
forgamesandstufjk-cloud <forgamesandstufjk@gmail.com>
2026-04-21 13:19:50 +0300

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', config('app.name', 'Laravel'))</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>

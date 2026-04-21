fe4790955906758db79197cfd0bbdc636bbf974b
forgamesandstufjk-cloud <forgamesandstufjk@gmail.com>
2026-04-21 14:39:17 +0300

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', config('app.name', 'Laravel'))</title>
+    <title>{{ isset($title) && trim((string) $title) !== '' ? $title : config('app.name', 'Laravel') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>

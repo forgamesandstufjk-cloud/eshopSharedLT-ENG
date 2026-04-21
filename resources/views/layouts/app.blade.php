e4b41e027ba75a22fc190d7670c872b2e26961a6
forgamesandstufjk-cloud <forgamesandstufjk@gmail.com>
2026-04-21 14:38:56 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="stripe-key" content="{{ config('services.stripe.key') }}">
 
-        <title>@yield('title', config('app.name', 'Laravel'))</title>
+       <title>{{ isset($title) && trim((string) $title) !== '' ? $title : config('app.name', 'Laravel') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />

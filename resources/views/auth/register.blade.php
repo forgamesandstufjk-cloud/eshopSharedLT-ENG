f7c5531256ff5ded6a6d91f54c38a88a0e70e4fc
forgamesandstufjk-cloud <forgamesandstufjk@gmail.com>
2026-05-20 22:30:11 +0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -107,7 +107,7 @@
             <button
                 type="submit"
                 class="text-white px-6 py-3 rounded hover:text-black"
-                style="background-color: rgb(131, 99, 84)"
+                style="background-color: rgb(104, 79, 67)"
             >
                 {{ __('Registruotis') }}
             </button>

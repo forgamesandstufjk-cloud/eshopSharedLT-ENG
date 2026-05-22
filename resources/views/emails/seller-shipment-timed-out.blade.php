e863ab802b3b96b46ca21ac62b524e2ef1d96e80
forgamesandstufjk-cloud <forgamesandstufjk@gmail.com>
2026-05-22 14:43:57 +0300

    Add files via upload

---
+++ resources/views/emails/seller-shipment-timed-out.blade.php
@@ -0,0 +1,12 @@
+<p>Sveiki,</p>
+
+<p>Siunta buvo automatiškai atšaukta, nes siuntos įrodymas nebuvo pateiktas per numatytą laiką.</p>
+
+<p>
+    Užsakymas #{{ $shipment->order_id }}<br>
+    Siunta #{{ $shipment->id }}
+</p>
+
+<p>Pirkėjui buvo atliktas grąžinimas.</p>
+
+<p>Jei tai įvyko per klaidą, susisiekite su administracija.</p>

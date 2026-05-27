86eb95cfa39c41f0af9a1db605e037b60499845a
forgamesandstufjk-cloud <forgamesandstufjk@gmail.com>
2026-05-27 17:38:36 +0300

    Update passwords.php

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Jūsų slaptažodis buvo pakeistas!',
+    'sent' => 'Slaptažodžio atkūrimo nuoroda išsiųsta el. paštu!',
+    'throttled' => 'Palaukite prieš bandydami dar kartą.',
+    'token' => 'Ši slaptažodžio atkūrimo nuoroda negalioja.',
+    'user' => 'Naudotojas su tokiu el. pašto adresu nerastas.',
 
 ];
